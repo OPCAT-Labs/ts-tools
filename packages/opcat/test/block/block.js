1a853fc3235cab0134546acc0c3470614f28e6f9
hh <hf.zheng@scrypt.io>
2025-07-02 16:59:01 +0800

    remove no use code

--- packages/opcat/test/block/block.js
+++ packages/opcat/test/block/block.js
@@ -11,9 +11,7 @@ var fs = require('fs');
 var should = chai.should();
 var Transaction = opcat.Transaction;
 
-// https://test-insight.bitpay.com/block/000000000b99b16390660d79fcc138d2ad0c89a0d044c4201a02bdf1f61ffa11
-var dataRawBlockBuffer = fs.readFileSync('test/data/blk86756-testnet.dat');
-var dataRawBlockBinary = fs.readFileSync('test/data/blk86756-testnet.dat', 'binary');
+
 var dataJson = fs.readFileSync('test/data/blk86756-testnet.json').toString();
 var data = require('../data/blk86756-testnet');
 var dataBlocks = require('../data/bitcoind/blocks');
