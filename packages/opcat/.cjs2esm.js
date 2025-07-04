6de642cd97cb99d5b30ac10c5b2476aa9ad608c1
hh <hf.zheng@scrypt.io>
2025-07-04 11:59:57 +0800

    add cjs2esm

---
+++ packages/opcat/.cjs2esm.js
@@ -0,0 +1,18 @@
+module.exports = {
+  input: ['cjs'],
+  ignore: [],
+  output: 'esm',
+  forceDirectory: null,
+  modules: [],
+  extension: {
+    use: 'js',
+    ignore: [],
+  },
+  addModuleEntry: false,
+  addPackageJson: true,
+  filesWithShebang: [],
+  codemod: {
+    path: '',
+    files: ['cjs', 'exports', 'named-export-generation'],
+  },
+};
